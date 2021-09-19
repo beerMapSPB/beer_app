f015571183314802e8be7aa70e41c8a1791af1f1
vladk <vlad.kuz12@gmail.com>
2021-09-20 04:34:17 +0500

    Add Places listView #9

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,8 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String incidentBeerShop = 'assets/images/IncidentBeerShop.jpg';
+  static const String stars35 = 'assets/images/Stars3-5.png';
+}
